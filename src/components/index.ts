01b089329d667cf69fefdc89c2bbb1b1afa38b92
devakaar <infoindore7@gmail.com>
2023-01-16 00:56:03 +0530

    code formating

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
-import BannerSlider from './BannerSlider';
 import Header from './Header';
 import SButton from './SButton';
 import Input from './Input';
 import BottomView from './BottomView';
 
-export {BannerSlider, SButton, Header, Input, BottomView};
+export {SButton, Header, Input, BottomView};
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -6,22 +6,13 @@ import {
   Text,
   Image,
   TouchableOpacity,
-  Button,
-  PermissionsAndroid,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {ConsultantApi} from '../../service';
 import {Colors} from '../../theme';
 import {DEVICE_HEIGHT, DEVICE_WIDTH} from '../../utils';
-import {BannerSlider} from '../../components';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-
-const bannerImages = [
-  {uri: 'https://picsum.photos/200/300'},
-  {uri: 'https://picsum.photos/200/400'},
-  {uri: 'https://picsum.photos/200/500'},
-];
 
 const Home = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStack>>();
@@ -37,46 +28,11 @@ const Home = () => {
       }
     };
     callApi();
-    requestCameraPermission();
   }, []);
-
-  const requestCameraPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: 'Cool Photo App Camera Permission',
-          message:
-            'Cool Photo App needs access to your camera ' +
-            'so you can take awesome pictures.',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can use the camera');
-      } else {
-        console.log('Camera permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
-
-  const onPressVideoCall = () => {
-    navigation.navigate('CallScreen');
-  };
 
   return (
     <View style={styles.parent}>
-      <View style={{backgroundColor: Colors.PRIMARY}}>
-        <View style={styles.banner}>
-          <BannerSlider data={bannerImages} />
-        </View>
-      </View>
       <Text style={styles.heading}>Top Rated Consultants</Text>
-      <Button title="Video Call " onPress={onPressVideoCall} />
       <FlatList
         keyExtractor={item => item._id}
         data={data}
